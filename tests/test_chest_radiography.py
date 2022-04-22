701e92b57fb5910fb3939e066d23c13ddbc6fbaa
Scott Sun <minghuisunscott@gmail.com>
2022-04-22 15:08:11 -0400

    start out testing

---
+++ tests/test_chest_radiography.py
@@ -0,0 +1,7 @@
+"""Test chest_radiography & db."""
+
+import sys
+
+sys.path.append("../final_project")  # noqa: E402
+
+import chest_radiography
---
+++ tests/test_utils.py
@@ -0,0 +1,12 @@
+"""Test RetinaNet/utils.py."""
+
+import sys
+import pytest
+import os
+
+sys.path.append("../final_project")  # noqa: E402
+import RetinaNet.utils as utils
+
+
+if __name__ == "__main__":
+    print(os.getcwd())
